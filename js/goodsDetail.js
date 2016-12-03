038cbcb02a3544083d301a900504e5958a8f906c
dudaxia <295374219@qq.com>
2016-12-03 18:18:22 +0800

    完善商品详情页面 套餐选择 数量改变

--- js/goodsDetail.js
+++ js/goodsDetail.js
@@ -1,12 +1,15 @@
 
 
+$(function(){
 
 	/**1添加公共样式topbar*/
 	$('.topbar-wrapper').load('topbar.html');
 
 	/**2三级导航*/
 
-	/**3放大镜效果*/
+	/**
+		3放大镜效果
+	*/
 	var imgGlass = {
 		sImgWrap: $('.zs-big-pic'),//小图片盒子
 		largeImgWrap: $('.zoom-big'),//大图片盒子
@@ -92,42 +95,204 @@
 	
 
 
-	/**4商品数量选择及购买或添加购物车*/
+	/**
+		4商品数量选择及购买或添加购物车
+	*/
 	var addGoods = {
-		package: $('.suit-name'),
-		increaseBtn: $('.increase'),
-		decreaseBtn: $('.decrease'),
-		goodsAmount: $('.goods-amount'),
-		addToCartBtn: $('.zs-store-buy'),
+		main: $('.zs-wrapper'),//商品最外层盒子
+		package: $('.suit-name'),//套餐选择
+		packageInfos: $( '.zp-suit-desc p' ),//套餐介绍
+		increaseBtn: $('.increase'),//加号
+		decreaseBtn: $('.decrease'),//减号
+		goodsAmountCont: $('.goods-amount'),//商品数量输入框
+		addToCartBtn: $('.zs-store-buy'),//添加至购物车按钮
+		amount: 0,//商品数量数字
+		data: {},//后台数据
+		packageWrapper: $('.suit-name-wrapper'),
 		init:function(){
+			//this.createPackage();
 			this.ChoicePackage();
 			this.increase();
 			this.decrease();
+			this.input();
 			this.addToCart();
+			
 		},
-		//套餐选择
-		ChoicePackage:function(){
+		/*//动态生成套餐
+		createPackage: function(){
 			var that = this;
-			this.package.click(function(){
+			//获取goods-id
+				var gid = this.main.attr( 'data-goodsid' );
+				gid = parseInt( gid );
+			//读取JSON数据
+			$.getJSON( 'js/cartdata.json',function( result ){
+				//获取数据
+				that.data = result;
+
+				var content = '';
+				//遍历package
+				for( var key in that.data[gid]['goods-package'] ){
+					content += '<a href="javascript:;" class="suit-name" data-goodsid="'+gid+'" data-package="'+key+'">'+that.data[gid]['goods-package'][key]+'<i class=""></i></a>' ;
+				}
+				//
+				that.packageWrapper.html(content);
+				//给第一个尺寸添加选中状态  children() 子元素
+				that.packageWrapper.children().eq(0).addClass('actived').find('i').addClass('actived');
+			} );
+		},*/
+		//套餐选择
+		ChoicePackage: function(){
+			var that = this;
+			this.package.click( function(){
 				//获取下标
 				var index = $(this).index();
-				$(this).addClass('selected').siblings().removeClass('selected');
+				//当前点击增加class
+				$(this).addClass('actived').siblings().removeClass('actived');
+				//给下标相同的i增加class
+				$(this).find('i').addClass('actived')
+						.parent().siblings().find('i').removeClass('actived');
+				//让相应套餐的简介显示
+				that.packageInfos.eq(index-1).show().siblings().hide();
 			});
 		},
 		//点击数量增加
 		increase:function(){
-
+			var that = this;
+			this.increaseBtn.click(function(){
+				//获取文本框的值
+				that.amount = parseInt( $(this).prev().val() );
+				//判断边界
+				if( that.amount >= 2 ){
+					$(this).prev().val(3) ;
+					return;
+				}
+				//处理数据
+				that.amount++;
+				$(this).prev().val(that.amount);
+			});
 		},
 		//点击数量减少
 		decrease: function(){
+			var that = this;
+			this.decreaseBtn.click(function(){
+				//获取文本框的值
+				that.amount = parseInt( $(this).next().val() );
+				//判断边界
+				if( that.amount <= 2 ){
+					$(this).next().val(1) ;
+					return;
+				}
+				//处理数据
+				that.amount--;
+				$(this).next().val(that.amount);
+			});
+		},
+		//直接输入时
+		input: function(){
+			var that = this;
 
+			this.goodsAmountCont.on( 'input',function(){
+				//获取输入的内容
+				var amount = $(this).val();
+				amount = parseInt( amount );
+				//判断是否大于限购数
+				if( amount >= 3 ){
+					$(this).val( 3 );
+					that.amount = 3;
+					return;
+				}
+				//判断输入的是否为非数字或者为0
+				if( isNaN(amount) || ( amount == 0 ) ){
+					$(this).val( 1 );
+				}else{
+					$(this).val( amount );
+				}
+				that.amount = amount;
+			} );
 		},
 		//点击加入购物车
 		addToCart: function(){
+			var that = this;
+			this.addToCartBtn.click(function(){
 
+			});
 		}
 	}; 
 	addGoods.init();
+
+	/*
+		详情 评价
+	*/
+	var tabbar = {
+		lis: $('.zs-tabbar li'),
+		contents: $('.zs-container .item'),
+		init: function(){
+			//初始化方法
+			this.choice();
+		},
+		choice: function(){
+			var that = this;
+			this.lis.click(function(){
+				//将点击的li添加class，并去除其他li的class
+				$(this).addClass('cur').siblings().removeClass('cur');
+				//获取当前的下标
+				var index = $(this).index();
+				//让与下标相同的内容区显示
+				that.contents.eq(index).show().siblings().hide();
+			});
+		}
+	};
+	//方法调用
+	tabbar.init();
+
+	
+});
+
+
+/*
+	//cookie
+	zolCart = {
+ 		1000101:{
+			"goods-id": 10001,
+			"goods-package": 1000101,
+			"package-price": 3000,
+			"amount": 3
+ 		},
+ 		1000102:{
+			"goods-id": 10001,
+			"goods-package": 1000102,
+			"package-price": 4000,
+			"amount": 3
+ 		}
+		
+ 	}
+
+ 	//数据库JSON
+ 	{
+	"10001":{
+		"shop-name":"瑞意摄影器材",
+		"shop-id": 20001,
+		"goods-id": 10001,
+		"goods-name": "NIKON尼康 DX 18-300mm f/3.5-6.3G ED VR 签约代理产品有保障",
+		"goods-color": "黑色",
+		"goods-package":{
+			"1000101": "官方标配",
+			"1000102": "套餐一",
+			"1000102": "套餐二",
+			"1000102": "套餐三",
+			"1000102": "套餐四"
+		},
+		"stock": 900
+	}
+}
+
+*/
+
+
+
+
+
+
 
 
 
